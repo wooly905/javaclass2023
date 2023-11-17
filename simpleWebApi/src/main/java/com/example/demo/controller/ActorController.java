ba8cc955d605f531047dd51eb6f2a485134bfc2a
bruce <>
2023-11-17 21:30:47 +0800

    add base path api prefix

--- simpleWebApi/src/main/java/com/example/demo/controller/ActorController.java
+++ simpleWebApi/src/main/java/com/example/demo/controller/ActorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.swing.text.html.Option;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api")
 public class ActorController {
 
     @Autowired
--- simpleWebApi/src/main/java/com/example/demo/controller/LanguageController.java
+++ simpleWebApi/src/main/java/com/example/demo/controller/LanguageController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api")
 public class LanguageController {
 
     @Autowired
