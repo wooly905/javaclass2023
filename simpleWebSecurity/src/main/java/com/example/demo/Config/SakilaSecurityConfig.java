dadbf25bfc9feda28604662ab97c509f3306db5b
bruce <>
2023-11-28 22:03:20 +0800

    update impl

--- simpleWebSecurity/src/main/java/com/example/demo/Config/SakilaSecurityConfig.java
+++ simpleWebSecurity/src/main/java/com/example/demo/Config/SakilaSecurityConfig.java
@@ -1,12 +1,10 @@
 package com.example.demo.Config;
 
-import com.example.demo.Services.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
@@ -17,11 +15,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SakilaSecurityConfig {
 
-    private final UserDetailsServiceImpl userDetailsService;
-
-    public SakilaSecurityConfig(UserDetailsServiceImpl userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
